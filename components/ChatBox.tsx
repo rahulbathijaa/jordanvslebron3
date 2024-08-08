dfc75efd80a27d6523e5bcde21c44d55a54cd2a8
Rahul <bathijarahul@gmail.com>
2024-08-08 19:46:17 -0400

    revision

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -16,7 +16,7 @@ const ChatBox: React.FC = () => {
     ]);
     try {
       console.log('Sending message:', messageToSend);
-      const response = await axios.post('/api/chat', { message: messageToSend }, { timeout: 120000 });
+      const response = await axios.post('https://jordanvslebron.vercel.app/api/chat', { message: messageToSend }, { timeout: 120000 });
       console.log('Received response:', response.data);
       if (response.data && response.data.response) {
         setChat(prevChat => [
