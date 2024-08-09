7fcd9680b8b269643299ba1ffd2fd59ff5dbe48a
Rahul <bathijarahul@gmail.com>
2024-08-08 20:25:02 -0400

    trying to revert?

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",  "http://localhost:8000", "https://jordanvslebron.com", "https://jordanvslebron.vercel.app"],  # Combined allow_origins
+    allow_origins=["http://localhost:3000", "http://localhost:8000", "https://jordanvslebron.com", "https://jordanvslebron.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -34,7 +34,7 @@ class Message(BaseModel):
 @modal_app.cls(
     gpu="a10g",
     image=llm_image,
-    volumes={"/root/model_cache": volume},  # Changed the volume mount path
+    volumes={"/root/model_cache": volume},
     mounts=[modal.Mount.from_local_dir(".", remote_path="/root/app")]
 )
 class LLMInference:
@@ -55,17 +55,16 @@ class LLMInference:
 
         inputs = self.tokenizer(message, return_tensors="pt", padding=True, truncation=True)
 
-        # Use torch.no_grad() to speed up inference
         with no_grad():
             outputs = self.model.generate(
                 inputs.input_ids,
                 attention_mask=inputs.attention_mask,
-                max_length=50,  # Reduced from 100 to 50
+                max_length=50,
                 num_return_sequences=1,
                 temperature=0.7,
                 do_sample=True,
-                top_p=0.9,  # Added top_p for nucleus sampling
-                num_beams=1,  # Using beam search with num_beams=1 for faster generation
+                top_p=0.9,
+                num_beams=1,
             )
 
         response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
@@ -76,11 +75,9 @@ llm = LLMInference()
 @app.post("/api/chat")
 async def chat_endpoint(message: Message):
     prompt = message.message.lower()
-    # Check for specific questions
-    if any(phrase in prompt for phrase in ["who's the goat", "whos the goat", "who is the goat", "who's the greatest", "whos the greates ever", "who is the greatest"]):
+    if any(phrase in prompt for phrase in ["who's the goat", "whos the goat", "who is the goat", "who's the greatest", "whos the greatest", "who is the greatest"]):
         return {"response": "That's for you to decide ;)"}
     else:
-        # Call the generate method without await
         response = llm.generate.remote(f"Human: {message.message}\nAI:")
         return {"response": response}
 
@@ -91,7 +88,7 @@ if os.getenv("VERCEL"):
 
 @modal_app.local_entrypoint()
 def main():
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
 if __name__ == "__main__":
     modal.run()
